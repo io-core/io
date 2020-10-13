b0a63bf2d42d33ad877c55b1c77d5909848caaa9
Charles Perkins <charlesap@gmail.com>
2020-10-13 08:13:25 -0700

    clean sweep preparing for submodules

--- images/old/core/misc/GatherOriginalProjectOberonFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir Sources
-curl https://www.inf.ethz.ch/personal/wirth/ProjectOberon/index.html | grep Mod.txt | sed -e 's/.*HREF=.\(.*\).txt\".*/curl https:\/\/www.inf.ethz.ch\/personal\/wirth\/ProjectOberon\/\1.txt > \1/g' | bash ; wget https://www.inf.ethz.ch/personal/wirth/ProjectOberon/license.txt 
-
-find -type f -name '*Mod.txt' | while read f; do mv "$f" "${f%.txt}"; done
-
--- images/old/rem/install_inner_kernel.sh
+++
@@ -1,1 +0,0 @@
-dd if=Modules.bin of=Oberon-2016-08-02.new conv=notrunc bs=1024 seek=1
