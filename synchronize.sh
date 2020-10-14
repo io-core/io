db838d9d1b3caece9289a2163adc9485d50c1c88
Charles Perkins <chuck@kuracali.com>
2020-10-14 07:25:14 -0700

    sync local to master

--- synchronize.sh
+++ synchronize.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+pushd .
+cd root/doc
+bash synchronize.sh
+popd
+for i in `ls -d root/src/github.com/io-core/*/`; do
+       pushd .	
+       cd $i
+       bash synchronize.sh
+       popd
+done
+git add images/io.img
 git add root/doc
 git add root/src/github.com/io-core/*
 git commit -m 'sync local to master'
