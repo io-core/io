17ab8518537d77210f39744521d6d6950593c667
Charles Perkins <chuck@kuracali.com>
2020-10-14 08:07:48 -0700

    sync local to master

--- synchronize.sh
+++ synchronize.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 pushd .
 cd root/doc
+echo "at `pwd`"
 bash synchronize.sh
 popd
+git add root/doc
 for i in `ls -d root/src/github.com/io-core/*/`; do
        pushd .	
        cd $i
+       echo "at `pwd`"
        bash synchronize.sh
        popd
+       git add $i
 done
 git add images/io.img
-git add root/doc
-git add root/src/github.com/io-core/*
+#git add root/src/github.com/io-core/*
 git commit -m 'sync local to master'
 git push origin main
