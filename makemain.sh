3407c01667595a6b1b4b037d9e1f168b9868aaf5
Charles Perkins <chuck@kuracali.com>
2021-08-28 07:55:44 -0700

    add charlesap submodules to makemain

--- makemain.sh
+++ makemain.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-pushd .
-cd root/doc
-echo "at `pwd`"
-git checkout main
-popd
 
 for i in `ls -d root/src/github.com/io-core/*/`; do
        pushd .	
        cd $i
        echo "at `pwd`"
        git checkout main
-       popd
-       
+       popd       
+done
+
+for i in `ls -d root/src/github.com/charlesap/*/`; do
+       pushd .	
+       cd $i
+       echo "at `pwd`"
+       git checkout main
+       popd       
 done
